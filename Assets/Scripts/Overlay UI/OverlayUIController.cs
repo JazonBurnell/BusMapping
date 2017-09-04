9d29cf4432acc8bcfdcb16ab61d74d1463750f35
JazonBurnell <jazon.burnell@gmail.com>
2017-09-04 15:17:15 -0800

    Starting on scrollable main view

--- Assets/Scripts/Overlay UI/OverlayUIController.cs
+++ Assets/Scripts/Overlay UI/OverlayUIController.cs
@@ -7,4 +7,15 @@ public class OverlayUIController : MonoBehaviour {
 	public BusMapUIController busMapUIController;
 
 	public TimelineBarUIController timelineBarUIController;
+
+	public Camera mapCamera;
+
+	public ScrollRect mapMainScrollView;
+
+	void LateUpdate () {
+		// camera x: 0.255
+		// mainscrollview.content.x: -278.2393
+
+		this.mapCamera.transform.localPosition = new Vector3(this.mapMainScrollView.content.anchoredPosition.x / -1091.13451f, this.mapMainScrollView.content.anchoredPosition.y / -1091.13451f, this.mapCamera.transform.localPosition.z);
+	}
 }
